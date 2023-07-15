610bb6e13f8a1b04f43e505131a8197b3956623f
Yossi Shapira <ys250264@ncr.com>
2023-07-15 07:51:06 +0300

    update CopyPosExtToCust.cmd

--- helpers/CopyPosExtToCust.cmd
+++ helpers/CopyPosExtToCust.cmd
@@ -10,8 +10,7 @@ set PosDebugExtPath=%2
 call .\helpers\killPOS.cmd
 
 :COPY
-xcopy %PosDebugExtPath%\Output\Debug\Product\*.* %PosDebugCustPath%\Output\Debug\Product\ /y /e 
-xcopy %PosDebugExtPath%\Output\Debug\Product\Plugins\*.* %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y /e 
+xcopy %PosDebugExtPath%\Output\Debug\Product\*.* %PosDebugCustPath%\Output\Debug\Product\ /y /s /e 
 xcopy %PosDebugExtPath%\Src\EPS\Retalix.Jumbo.Client.EPS.Simulator\obj\Debug\Retalix.Jumbo.Client.EPS.Simulator.dll %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y
 
 :END
