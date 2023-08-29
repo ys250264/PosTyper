373282b185ab2bacc31a5b17f8bd7e281848f2cb
Yossi Shapira <ys250264@ncr.com>
2023-08-29 14:09:05 +0300

    modify FixRTIs after SME created 5_PostData folder

--- helpers/FixRTIs.cmd
+++ helpers/FixRTIs.cmd
@@ -10,7 +10,6 @@ set TargetBase=%ServerDebugExtPath%\%RTIsPath%
 set SourceBase=.\helpers
 set Configuration=1_Configuration
 set LabSpecific=3_Lab_IL_Specific
-set RunLast=9_Run_Last
 
 :COPY_RTIS_WITH_MACHINE_NAME
 xcopy %SourceBase%\%LabSpecific%\*.* %TargetBase%\%LabSpecific%\ /y /s
@@ -19,9 +18,7 @@ xcopy %SourceBase%\%LabSpecific%\*.* %TargetBase%\%LabSpecific%\ /y /s
 set FileToRemove=%TargetBase%\%Configuration%\028_ConfigurationEntry\*.RUN_ON_ECOMM_ONLY
 if exist %FileToRemove% del /f /q %FileToRemove%
 
-:RUN_LAST
-set FolderToMove=1_Dev\090_EligibilityPolicy
-robocopy %TargetBase%\%LabSpecific%\%FolderToMove% %TargetBase%\%RunLast%\%FolderToMove% /MOVE /NFL /NDL /NJH /NJS /nc /ns /np
-
 :END
-pause
+Echo:
+Echo:
+IF %ERRORLEVEL% NEQ 0 pause
