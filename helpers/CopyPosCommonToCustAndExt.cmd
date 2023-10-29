0d2a22a6378a93aa03355ad3deb2e1210ffca062
Yossi Shapira <ys250264@ncr.com>
2023-10-29 10:03:01 +0200

    prettify CopyPosCommonToCustAndExt.cmd output

--- helpers/CopyPosCommonToCustAndExt.cmd
+++ helpers/CopyPosCommonToCustAndExt.cmd
@@ -13,38 +13,38 @@ set ClientPresentationCore=Retalix.Client.Presentation.Core
 :COPY_COMMON_MyEventLog_TO_CUST_SRC
 Echo:
 Echo ********** Copy Common to Cust Src **************************************************************************************************************************
-xcopy %CommonDebugCustPath%\%ClientCommon%.dll %PosDebugCustPath%\SharedLibs\ClientCommon\ /y
-xcopy %CommonDebugCustPath%\%ClientCommon%.pdb %PosDebugCustPath%\SharedLibs\ClientCommon\ /y
-xcopy %CommonDebugCustPath%\%ClientNet%.dll %PosDebugCustPath%\SharedLibs\ClientCommon\ /y
-xcopy %CommonDebugCustPath%\%ClientNet%.pdb %PosDebugCustPath%\SharedLibs\ClientCommon\ /y
-xcopy %CommonDebugCustPath%\%ClientPresentationCore%.dll %PosDebugCustPath%\SharedLibs\ClientCommon\ /y
-xcopy %CommonDebugCustPath%\%ClientPresentationCore%.pdb %PosDebugCustPath%\SharedLibs\ClientCommon\ /y
+xcopy %CommonDebugCustPath%\%ClientCommon%.dll %PosDebugCustPath%\SharedLibs\ClientCommon\ /y | findstr /v "1 File(s) copied" 
+xcopy %CommonDebugCustPath%\%ClientCommon%.pdb %PosDebugCustPath%\SharedLibs\ClientCommon\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientNet%.dll %PosDebugCustPath%\SharedLibs\ClientCommon\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientNet%.pdb %PosDebugCustPath%\SharedLibs\ClientCommon\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientPresentationCore%.dll %PosDebugCustPath%\SharedLibs\ClientCommon\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientPresentationCore%.pdb %PosDebugCustPath%\SharedLibs\ClientCommon\ /y | findstr /v "1 File(s) copied"
 
 :COPY_COMMON_MyEventLog_TO_EXT_SRC
 Echo:
 Echo ********** Copy Common to Ext Src **************************************************************************************************************************
-xcopy %CommonDebugCustPath%\%ClientCommon%.dll %PosDebugExtPath%\Libs\Retalix\POSClient\ /y
-xcopy %CommonDebugCustPath%\%ClientCommon%.pdb %PosDebugExtPath%\Libs\Retalix\POSClient\ /y
-xcopy %CommonDebugCustPath%\%ClientNet%.dll %PosDebugExtPath%\Libs\Retalix\POSClient\ /y
-xcopy %CommonDebugCustPath%\%ClientNet%.pdb %PosDebugExtPath%\Libs\Retalix\POSClient\ /y
-xcopy %CommonDebugCustPath%\%ClientPresentationCore%.dll %PosDebugExtPath%\Libs\Retalix\POSClient\ /y
-xcopy %CommonDebugCustPath%\%ClientPresentationCore%.pdb %PosDebugExtPath%\Libs\Retalix\POSClient\ /y
+xcopy %CommonDebugCustPath%\%ClientCommon%.dll %PosDebugExtPath%\Libs\Retalix\POSClient\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientCommon%.pdb %PosDebugExtPath%\Libs\Retalix\POSClient\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientNet%.dll %PosDebugExtPath%\Libs\Retalix\POSClient\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientNet%.pdb %PosDebugExtPath%\Libs\Retalix\POSClient\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientPresentationCore%.dll %PosDebugExtPath%\Libs\Retalix\POSClient\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientPresentationCore%.pdb %PosDebugExtPath%\Libs\Retalix\POSClient\ /y | findstr /v "1 File(s) copied"
 
 :COPY_COMMON_MyEventLog_TO_OUTPUT
 Echo:
 Echo ********** Copy Common to Cust Output **************************************************************************************************************************
-xcopy %CommonDebugCustPath%\%ClientCommon%.dll %PosDebugCustPath%\Output\Debug\Product\ /y
-xcopy %CommonDebugCustPath%\%ClientCommon%.pdb %PosDebugCustPath%\Output\Debug\Product\ /y
-xcopy %CommonDebugCustPath%\%ClientCommon%.dll %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y
-xcopy %CommonDebugCustPath%\%ClientCommon%.pdb %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y
-xcopy %CommonDebugCustPath%\%ClientNet%.dll %PosDebugCustPath%\Output\Debug\Product\ /y
-xcopy %CommonDebugCustPath%\%ClientNet%.pdb %PosDebugCustPath%\Output\Debug\Product\ /y
-xcopy %CommonDebugCustPath%\%ClientNet%.dll %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y
-xcopy %CommonDebugCustPath%\%ClientNet%.pdb %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y
-xcopy %CommonDebugCustPath%\%ClientPresentationCore%.dll %PosDebugCustPath%\Output\Debug\Product\ /y
-xcopy %CommonDebugCustPath%\%ClientPresentationCore%.pdb %PosDebugCustPath%\Output\Debug\Product\ /y
-xcopy %CommonDebugCustPath%\%ClientPresentationCore%.dll %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y
-xcopy %CommonDebugCustPath%\%ClientPresentationCore%.pdb %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y
+xcopy %CommonDebugCustPath%\%ClientCommon%.dll %PosDebugCustPath%\Output\Debug\Product\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientCommon%.pdb %PosDebugCustPath%\Output\Debug\Product\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientCommon%.dll %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientCommon%.pdb %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientNet%.dll %PosDebugCustPath%\Output\Debug\Product\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientNet%.pdb %PosDebugCustPath%\Output\Debug\Product\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientNet%.dll %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientNet%.pdb %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientPresentationCore%.dll %PosDebugCustPath%\Output\Debug\Product\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientPresentationCore%.pdb %PosDebugCustPath%\Output\Debug\Product\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientPresentationCore%.dll %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y | findstr /v "1 File(s) copied"
+xcopy %CommonDebugCustPath%\%ClientPresentationCore%.pdb %PosDebugCustPath%\Output\Debug\Product\Plugins\ /y | findstr /v "1 File(s) copied"
 
 :END
 rem pause
